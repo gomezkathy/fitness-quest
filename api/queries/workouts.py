5bb9a6d8ded34194a59dff7eb27c440c222da81b
kathy <gomezkathy1997@gmail.com>
2023-08-24 14:14:42 -0700

    GET all workouts added and working

--- api/queries/workouts.py
+++ api/queries/workouts.py
@@ -1,8 +1,10 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List, Union
 from datetime import datetime, date
 from pool import pool
 
+class Error(BaseModel):
+    message: str
 
 class WorkoutIn(BaseModel):
     name: str
@@ -11,7 +13,6 @@ class WorkoutIn(BaseModel):
     reps: Optional[int]
     picture_url: Optional[str]
     description: Optional[str]
-    created_at: Optional[datetime]
     assigned_date: Optional[date]
 
 
@@ -23,11 +24,38 @@ class WorkoutOut(BaseModel):
     reps: Optional[int]
     picture_url: Optional[str]
     description: Optional[str]
-    created_at: Optional[datetime]
     assigned_date: Optional[date]
 
 
 class WorkoutRepository:
+    def get_all(self) -> Union[Error, List[WorkoutOut]]:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
+                        SELECT id, name, weight, sets, reps, picture_url, description, assigned_date
+                        FROM exercises
+                        ORDER BY assigned_date
+                        """
+                    )
+                    result = []
+                    for record in db:
+                        workout = WorkoutOut(
+                            id=record[0],
+                            name=record[1],
+                            weight=record[2],
+                            sets=record[3],
+                            reps=record[4],
+                            picture_url=record[5],
+                            description=record[6],
+                            assigned_date=record[7]
+                        )
+                        result.append(workout)
+                    return result
+        except Exception:
+            return {"message": "Could not get all comments"}
+
     def create(self, workout:WorkoutIn) -> WorkoutOut:
         with pool.connection() as conn:
             with conn.cursor() as db:
