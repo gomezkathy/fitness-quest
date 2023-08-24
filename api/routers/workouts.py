2283a3c8cd2378a25301c135223a2a41e8253577
Julius Newell <newelljulius@gmail.com>
2023-08-24 09:55:17 -0700

    protected endpoint for CREATE workout

--- api/routers/workouts.py
+++ api/routers/workouts.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from queries.workouts import WorkoutIn, WorkoutRepository
 from authenticator import authenticator
 
@@ -7,6 +7,12 @@ router = APIRouter()
 
 @router.post("/workouts")
 async def create_workout(
-    workout: WorkoutIn, repo: WorkoutRepository = Depends(authenticator.get_current_account_data)
+    workout: WorkoutIn,  # sets the workout model
+    account_data: dict = Depends(authenticator.get_current_account_data),  # verifies user authentication
+    repo: WorkoutRepository = Depends()  # creates an instance of WorkoutRepository
 ):
-    return repo.create(workout)
+    # if user is authenticated, creates workout. Otherwise, returns 401 error
+    if account_data:
+        return repo.create(workout)
+    else:
+        raise HTTPException(status_code=401, detail="User is not authenticated.")
