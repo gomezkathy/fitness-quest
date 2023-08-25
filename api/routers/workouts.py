78f6f2115f8c5f8f58a8b0317277aee3dbcb2834
Kathy Gomez <gomezkathy1997@gmail.com>
2023-08-25 22:12:24 +0000

    "Add POST,GET,PUT protected endpoints"

--- api/routers/workouts.py
+++ api/routers/workouts.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter, Depends, HTTPException
-from queries.workouts import WorkoutIn, WorkoutRepository
+from queries.workouts import WorkoutIn, WorkoutOut, WorkoutRepository, Error
 from authenticator import authenticator
+from typing import List, Union
 
 router = APIRouter()
 
 
-@router.post("/workouts")
+@router.post("/workouts", response_model=Union[WorkoutOut, Error])
 async def create_workout(
     workout: WorkoutIn,  # sets the workout model
     # verifies user authentication
@@ -17,5 +18,27 @@ async def create_workout(
     if account_data:
         return repo.create(workout)
     else:
-        raise HTTPException(status_code=401,
-                            detail="User is not authenticated.")
+        raise HTTPException(status_code=401, detail="User is not authenticated.")
+
+@router.get("/workouts", response_model=Union[Error, List[WorkoutOut]])
+async def get_all(
+   account_data: dict = Depends(authenticator.get_current_account_data),
+   repo: WorkoutRepository = Depends(),
+):
+    if account_data:
+        return repo.get_all()
+    else:
+      raise HTTPException(status_code=401, detail="User is not authenticated.")
+
+
+@router.put("/workouts/{workout_id}", response_model=Union[WorkoutOut, Error])
+async def update_workout(
+    workout_id: int,
+    workout: WorkoutIn,
+    account_data: dict = Depends(authenticator.get_current_account_data),
+    repo: WorkoutRepository = Depends(),
+) -> Union[Error, WorkoutOut]:
+    if account_data:
+        return repo.update(workout_id, workout)
+    else:
+        raise HTTPException(status_code=401, detail="User is not authenticated.")
