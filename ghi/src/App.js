a80f617727927aff435158bdc9591a81a9a900a8
Dakota <dakotasteppe@gmail.com>
2023-08-26 15:56:21 -0700

    browser router

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,20 +1,22 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 // import { Box } from "@mui/material";
 import NavBar from ',/components/NavBar';
 import './App.css'
 export default funtion App() {
   return (
-    <div className='App'>
-    <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="Workouts">
-          <Route index element={<Workouts />} />
-          <Route path="Create" element={<CreateWorkout />} />
-          <Route path="Update" element={<UpdateWorkout />} />
-        </Route>
-      </Routes>
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+      <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="Workouts">
+            <Route index element={<Workouts />} />
+            <Route path="Create" element={<CreateWorkout />} />
+            <Route path="Update" element={<UpdateWorkout />} />
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
