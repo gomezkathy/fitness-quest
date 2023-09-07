aab611411949753e259d224f1ef4a20071cfcbe2
Kathy Gomez <gomezkathy1997@gmail.com>
2023-09-07 22:16:16 +0000

    Resolve "update + delete frontend components as well as exercise list view"

--- api/routers/exercises.py
+++ api/routers/exercises.py
@@ -1,10 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException
-from queries.exercises import (
-    ExerciseIn,
-    ExerciseOut,
-    ExerciseRepository,
-    Error,
-)
+from queries.exercises import ExerciseIn, ExerciseOut, ExerciseRepository, Error
 from authenticator import authenticator
 from typing import List, Union, Optional
 
@@ -14,36 +9,28 @@ router = APIRouter()
 @router.post("/api/exercises", response_model=Union[ExerciseOut, Error])
 async def create_exercise(
     exercise: ExerciseIn,  # sets the exercise model
-    account_data: dict = Depends(
-        authenticator.get_current_account_data
-    ),  # verifies user authentication
-    repo: ExerciseRepository = Depends(),  # creates an instance of exerciseRepository
+    account_data: dict = Depends(authenticator.get_current_account_data),  # verifies user authentication
+    repo: ExerciseRepository = Depends()  # creates an instance of exerciseRepository
 ):
     # if user is authenticated, creates exercise. Otherwise, returns 401 error
     if account_data:
         return repo.create(exercise)
     else:
-        raise HTTPException(
-            status_code=401, detail="User is not authenticated."
-        )
-
+        raise HTTPException(status_code=401, detail="User is not authenticated.")
 
 @router.get("/api/exercises", response_model=Union[Error, List[ExerciseOut]])
 async def get_all(
-    account_data: dict = Depends(authenticator.get_current_account_data),
-    repo: ExerciseRepository = Depends(),
+   account_data: dict = Depends(authenticator.get_current_account_data),
+   repo: ExerciseRepository = Depends(),
 ):
+    # if user is authenticated, creates exercise. Otherwise, returns 401 error
     if account_data:
         return repo.get_all()
     else:
-        raise HTTPException(
-            status_code=401, detail="User is not authenticated."
-        )
+      raise HTTPException(status_code=401, detail="User is not authenticated.")
 
 
-@router.put(
-    "/api/exercises/{exercise_id}", response_model=Union[ExerciseOut, Error]
-)
+@router.put("/api/exercises/{exercise_id}", response_model=Union[ExerciseOut, Error])
 async def update_exercise(
     exercise_id: int,
     exercise: ExerciseIn,
@@ -53,9 +40,7 @@ async def update_exercise(
     if account_data:
         return repo.update(exercise_id, exercise)
     else:
-        raise HTTPException(
-            status_code=401, detail="User is not authenticated."
-        )
+        raise HTTPException(status_code=401, detail="User is not authenticated.")
 
 
 @router.delete("/api/exercises/{exercise_id}", response_model=bool)
@@ -67,14 +52,9 @@ async def delete_exercise(
     if account_data:
         return repo.delete(exercise_id)
     else:
-        raise HTTPException(
-            status_code=401, detail="User is not authenticated."
-        )
+        raise HTTPException(status_code=401, detail="User is not authenticated.")
 
-
-@router.get(
-    "/api/exercises/{exercise_id}", response_model=Optional[ExerciseOut]
-)
+@router.get("/api/exercises/{exercise_id}", response_model=Optional[ExerciseOut])
 async def get_one_exercise(
     exercise_id: int,
     account_data: dict = Depends(authenticator.get_current_account_data),
@@ -83,6 +63,4 @@ async def get_one_exercise(
     if account_data:
         return repo.get_one(exercise_id)
     else:
-        raise HTTPException(
-            status_code=401, detail="User is not authenticated."
-        )
+        raise HTTPException(status_code=401, detail="User is not authenticated.")
--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,29 +1,33 @@
-import React from "react";
+import React, { useEffect, useState }  from "react";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
-import Footer from "./components/Footer";
+
 import "./App.css";
-import { AuthProvider } from "@galvanize-inc/jwtdown-for-react";
+
 
 // AUTH
-import LoginForm from "./Accounts/LoginForm";
-import Logout from "./Accounts/Logout";
-import SignupForm from "./Accounts/SignupForm";
+
 
 // WORKOUTS
 import Workouts from "./pages/Workouts";
 import CreateWorkout from "./pages/CreateWorkout";
 
 // EXERCISES
-import ExerciseForm from "./pages/CreateExercise";
-import Exercises from "./pages/Exercises";
+
+
 
 // COMMENTS
 import Comments from "./pages/Comments";
 import CommentForm from "./pages/CreateComment";
+import ExerciseForm from "./pages/CreateExercise";
+import ExerciseList from "./pages/ExercisesList";
+import UpdateExerciseForm from "./pages/UpdateExerciseForm";
+import { AuthProvider } from "@galvanize-inc/jwtdown-for-react";
 
 export default function App() {
+
+
   return (
     <AuthProvider baseUrl={process.env.REACT_APP_API_HOST}>
       <BrowserRouter>
@@ -31,9 +35,6 @@ export default function App() {
           <NavBar />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/signup" element={<SignupForm />} />
-            <Route path="/login" element={<LoginForm />} />
-            <Route path="/logout" element={<Logout />} />
             <Route path="/workouts">
               <Route index element={<Workouts />} />
               <Route path="create" element={<CreateWorkout />} />
@@ -43,8 +44,9 @@ export default function App() {
               <Route path="create" element={<CommentForm />} />
             </Route>
             <Route path="/exercises">
-              <Route index element={<Exercises />} />
+              <Route index element={<ExerciseList/>}/>
               <Route path="create" element={<ExerciseForm />} />
+              <Route path="update/:exerciseId" element={<UpdateExerciseForm />} />
             </Route>
           </Routes>
         </div>
