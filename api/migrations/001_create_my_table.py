9278b2ed7a874c33c18293b11e35312fd2da1899
Amos Pak <amos.pak7@gmail.com>
2023-09-07 22:53:07 +0000

    Resolve "Update Account User Information on frontend/backend, included a unit test, and also include a simple get user by ID for accounts"

--- api/migrations/001_create_my_table.py
+++ api/migrations/001_create_my_table.py
@@ -6,8 +6,8 @@ steps = [
             id SERIAL NOT NULL PRIMARY KEY,
             username VARCHAR(100) NOT NULL UNIQUE,
             password VARCHAR(100) NOT NULL,
-            first_name VARCHAR(100) NOT NULL,
-            last_name VARCHAR(100) NOT NULL,
+            first VARCHAR(100) NOT NULL,
+            last VARCHAR(100) NOT NULL,
             email VARCHAR(100) NOT NULL UNIQUE
         );
         """,
